cd126b79289a0299ec503b9b35f4775ca7109b78
calebjenkins <caleb@calebjenkins.com>
2021-02-12 09:56:16 -0600

    initial commit

---
+++ Learning.Domain/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.Domain
+{
+	public class Calculator
+	{
+		public decimal Add(decimal Num1, decimal Num2)
+		{
+			return Num1 + Num2;
+		}
+	}
+}
---
+++ Learning.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,68 @@
+﻿using Learning.Domain;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Learning.Specs.Steps
+{
+	[ExcludeFromCodeCoverage]
+	[Binding]
+	public sealed class CalculatorStepDefinitions
+	{
+		internal class TestCtx
+		{
+			public decimal First { get; set; }
+			public decimal Second { get; set; }
+			public decimal Result { get; set; }
+		}
+
+		// internal Tuple<decimal? first, decimal? second, decimal?> _ctx = new Tuple<decimal?, decimal?, decimal?>(null, null, null);
+		// internal Tuple<decimal? first, decimal? second, decimal?> ctx = (decimal? first, decimal? second, decimal? result); 
+
+		// For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+		private readonly ScenarioContext _scenarioContext;
+		private TestCtx _ctx = new TestCtx();
+
+		public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+		{
+			_scenarioContext = scenarioContext;
+		}
+
+		[Given("the first number is (.*)")]
+		public void GivenTheFirstNumberIs(decimal number)
+		{ 
+			_scenarioContext.Add("first", number);
+			_ctx.First = number;
+		}
+
+		[Given("the second number is (.*)")]
+		public void GivenTheSecondNumberIs(decimal number)
+		{
+			_scenarioContext.Add("second", number);
+			_ctx.Second = number;
+		}
+
+		[When("the two numbers are added")]
+		public void WhenTheTwoNumbersAreAdded()
+		{
+			var calc = new Calculator();
+			var result = calc.Add((decimal)_scenarioContext["first"], (decimal) _scenarioContext["second"]);
+
+			_ctx.Result = calc.Add(_ctx.First, _ctx.Second);
+			
+			_scenarioContext.Add("result", result);
+		}
+
+		[Then("the result should be (.*)")]
+		public void ThenTheResultShouldBe(int result)
+		{
+			//TODO: implement assert (verification) logic
+			var answer = (decimal) _scenarioContext["result"];
+			answer.Should().Be(result);
+
+			_ctx.Result.Should().Be(result);
+		}
+	}
+}
