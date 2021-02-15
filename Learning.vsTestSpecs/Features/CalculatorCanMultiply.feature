606a102b6efa155a766a9d5716a5065e219ca443
calebjenkins <caleb@calebjenkins.com>
2021-02-15 13:38:31 -0600

    adding second MsTesat feature. Livingdoc still working

---
+++ Learning.vsTestSpecs/Features/CalculatorCanMultiply.feature
@@ -0,0 +1,11 @@
+﻿Feature: Calculator can multiply
+![Calculator](https://specflow.org/wp-content/uploads/2020/09/calculator.png)
+Simple calculator for multiplying **two** numbers
+
+@msTest
+Scenario: Can multiply two numbers
+Given the first number is 10
+And the second number is 5
+When the numbers are multiplied
+Then the result should be 50
+
---
+++ Learning.vsTestSpecs/Steps/CalculatorCanMultiplySteps.cs
@@ -0,0 +1,24 @@
+﻿using Learning.Domain;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+using static Learning.vsTestSpecs.Steps.CalculatorStepDefinitions;
+
+namespace Learning.vsTestSpecs.Steps
+{
+	[Binding]
+	public class CalculatorCanMultiplySteps
+	{
+		private readonly CalculatorScenarioContext _ctx;
+		public CalculatorCanMultiplySteps(CalculatorScenarioContext TestContext)
+		{
+			_ctx = TestContext;
+		}
+
+		[When(@"the numbers are multiplied")]
+		public void WhenTheNumbersAreMultiplied()
+		{
+			var calc = new Calculator();
+			_ctx.Result = calc.Multiply(_ctx.First, _ctx.Second);
+		}
+	}
+}
