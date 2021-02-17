95ba01745245593cb8a67847e28570a333e01121
calebjenkins <caleb@calebjenkins.com>
2021-02-17 13:00:01 -0600

    Updated report scripts - all work now!

--- Reports/generateMsTestReport.ps1
+++ Reports/generateMsTestReport.ps1
@@ -1,5 +1,15 @@
-livingdoc feature-folder ..\Learning.vsTestSpecs  -t ..\Learning.vsTestSpecs\bin\Debug\net5.0\TestExecution.json
 
-$fileName = "LivingDoc.html"
+#set for runner/namespace
+$testRunner = "vsTest"
 
-explorer.exe $fileName
+#fixed variables
+$reportName = "LivingDoc.html"
+$featureFolder = "..\Learning." + $testRunner + "Specs"
+$testExecutionFile = "..\Learning." + $testRunner + "Specs\bin\Debug\net5.0\TestExecution.json"
+
+#generate Report
+livingdoc feature-folder $featureFolder -t $testExecutionFile 
+
+#rename generated file
+move .\$reportName .\$testRunner-$reportName -Force
+explorer.exe .\$testRunner-$reportName
